881a8cb5e02e21718756e0f2936cbb6f68187d15
Weeble <weeble69@protonmail.com>
2026-09-06 00:34:08 -0500

    fix screenshare

--- modules/nixos/xdg.nix
+++ modules/nixos/xdg.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 
 let
   niriScreenshare = inputs.niri-screenshare.packages.${pkgs.stdenv.hostPlatform.system}.default;
@@ -14,16 +14,25 @@ in
     ];
 
     config.umbriel = {
-      default = [ "umbriel" "gtk" ];
+      default = [
+        "umbriel"
+        "gtk"
+      ];
       "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
       "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
     };
 
     config.niri = {
       default = [ "gtk" ];
-      "org.freedesktop.impl.portal.ScreenCast" = [ "niri-screenshare" ];
+      "org.freedesktop.impl.portal.ScreenCast" = [ "niri" ];
       "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
       "org.freedesktop.impl.portal.Access" = [ "gtk" ];
     };
+  };
+
+  systemd.user.services.niri-screenshare = {
+    path = [ config.programs.niri.package ];
+    environment.NIRI_SCREENSHARE_PICKER = "1";
+    wantedBy = lib.mkForce [ "niri.service" ];
   };
 }
