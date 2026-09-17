a8e74cb098585ddfd9921614e2208aa0acb4f944
Weeble <weeble69@protonmail.com>
2026-09-17 15:45:47 -0500

    Update umbriel stuff

--- home/umbriel/layout.nix
+++ home/umbriel/layout.nix
@@ -5,14 +5,14 @@
     layout = {
       mode = "scrolling";
       gap = 4;
-      width_presets = [
+      extent_presets = [
         0.333
         0.5
         0.667
       ];
 
       scrolling = {
-        default_width_fraction = 0.5;
+        default_extent_fraction = 0.5;
         center_underfull_strip = false;
       };
     };
--- home/umbriel/rules.nix
+++ home/umbriel/rules.nix
@@ -3,12 +3,8 @@
 {
   programs.umbriel.settings.window_rule = [
     {
-      match.app_id = "^dev\\.noctalia\\.UmbrielSharePicker$";
-      default_floating = true;
-      default_size = [
-        800
-        600
-      ];
+      match.is_floating = true;
+      opacity = 0.95;
     }
 
     {
@@ -28,12 +24,41 @@
     }
 
     {
-      match.app_id = "xdg-desktop-portal-gtk";
+      match.app_id = "^(xdg-desktop-portal|dev\\.noctalia\\.UmbrielSharePicker)$";
       default_floating = true;
-      default_size = [
-        800
-        600
-      ];
+      default_floating_size_px = {
+        width = 800;
+        height = 600;
+      };
+    }
+
+    {
+      match.app_id = "^dev\\.noctalia\\.Noctalia$";
+      default_floating = true;
+      default_floating_size_px = {
+        width = 1020;
+        height = 900;
+      };
+    }
+
+    {
+      match.title = "^notificationtoasts_.+_desktop";
+      default_focused = false;
+      default_pinned = true;
+      default_position = {
+        x = 0;
+        y = 0;
+        anchor = "bottom_right";
+      };
+    }
+
+    {
+      match.title = "^(Open File|Select|Choose a wallpaper|Open Folder|Save As|Library|Choose Where to Download|File Operation Progress|Rename|Copy Files|Move Files|Search Files|File Upload)$";
+      default_floating = true;
+      default_floating_size_px = {
+        width = 800;
+        height = 600;
+      };
     }
   ];
 }
